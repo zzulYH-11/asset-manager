dd4fa4481f24f0bc087c258e0a10dff666ce29a1
zzulYH-11 <jjulu0227@naver.com>
2026-03-16 23:45:10 +0900

    feat : 프로젝트 초기 설정 및 DB 연결 완료

---
+++ src/main/java/com/yuha/asset_manager/AssetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.yuha.asset_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuha/asset_manager/AssetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuha.asset_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
